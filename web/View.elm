3614dc68f649887d794fff96cca56e3362cdc39c
Bernhard Mayr <bernhard@mayr.io>
2017-06-13 09:43:26 +0200

    Dialog working

--- web/View.elm
+++ web/View.elm
@@ -32,9 +32,27 @@ selectionToVisibility selected =
     ]
 
 
+playerColumns : Int
+playerColumns =
+    4
+
+
+playerPadding : Float
+playerPadding =
+    5
+
+
 renderPlayer : Model.Selectable Model.Player -> Html Msg
 renderPlayer selectable =
-    li [ styles ([ Css.width (Css.pct 23), Css.margin (Css.px 4), Css.position Css.relative ] ++ selectionOpacity selectable) ]
+    li
+        [ styles
+            ([ Css.property "width" ("calc(" ++ toString (100 // playerColumns) ++ "% - " ++ toString (2 * playerPadding) ++ "px)")
+             , Css.margin (Css.px playerPadding)
+             , Css.position Css.relative
+             ]
+                ++ selectionOpacity selectable
+            )
+        ]
         [ case selectable.object of
             Model.Employee employee ->
                 renderEmployee employee
@@ -78,42 +96,119 @@ pct100 =
 
 header : Html Msg
 header =
-    Html.header [ styles [ Css.backgroundColor white, Css.padding (Css.rem 1), Css.displayFlex, Css.justifyContent Css.center, Css.alignItems Css.center ] ]
-        [ img [ styles [ Css.height (Css.rem 4) ], src "./assets/softaware-icon.png" ] []
-        , Html.h1 [ styles [ Css.height (Css.rem 2), Css.margin2 Css.zero (Css.rem 0.2), Css.textTransform Css.uppercase, Css.fontSize (Css.rem 1.5), Css.fontFamily Css.sansSerif ] ] [ text "▷" ]
-        , img [ styles [ Css.height (Css.rem 4) ], src "./assets/soccer.png" ] []
+    Html.header [ styles [ Css.backgroundColor (Css.hex "f9f9f9"), Css.padding (Css.vh 2.2), Css.displayFlex, Css.justifyContent Css.center, Css.alignItems Css.center, Css.boxShadow4 Css.zero (Css.px 3) (Css.px 6) (Css.rgba 0 0 0 0.17) ] ]
+        [ img [ styles [ Css.height (Css.vh 3) ], src "./assets/randomzr.png" ] []
         ]
 
 
 view : Model -> Html Msg
 view model =
-    div [ styles [ Css.boxSizing Css.borderBox, Css.backgroundImage (Css.url "./assets/background.jpg"), Css.backgroundPosition Css.top, Css.backgroundSize Css.cover, Css.height (Css.vh 100), Css.width (Css.vw 100) ] ]
+    div [ styles [ Css.boxSizing Css.borderBox, Css.height (Css.vh 100), Css.width (Css.vw 100) ] ]
         [ errorMessage model.error
-        , header
-        , div []
-            [ Html.header
-                [ styles [ Css.height (Css.rem 7) ] ]
-                []
+        , lineUp model.lineUp
+        , div [ styles [ Css.displayFlex, Css.flexDirection Css.column ] ]
+            [ header
+            , Loading.spinner
+                [ Loading.active model.loading ]
+            , ul [ styles [ Css.listStyleType Css.none, Css.margin (Css.rem 1), Css.padding Css.zero, Css.displayFlex, Css.flexWrap Css.wrap, Css.justifyContent Css.center ] ]
+                (List.map renderPlayer (Model.orderedPlayers model))
+            , button
+                [ disabled (not (Model.isRandomizable model))
+                , onClick Randomize
+                , styles
+                    [ Css.alignSelf Css.center
+                    , Css.margin (Css.rem 2.5)
+                    , Css.backgroundColor (Css.rgb 119 179 0)
+                    , Css.color white
+                    , Css.textTransform Css.uppercase
+                    , Css.border Css.zero
+                    , Css.fontSize (Css.rem 1.5)
+                    , Css.padding2 (Css.rem 1) (Css.rem 4)
+                    , Css.borderRadius (Css.px 2)
+                    , Css.boxShadow5 Css.zero (Css.px 2) (Css.px 5) Css.zero (Css.rgba 0 0 0 0.26)
+                    , Css.property "transition" "box-shadow 0.2s cubic-bezier(0.4, 0, 0.2, 1)"
+                    , Css.hover
+                        [ Css.color (Css.hex "ff0000")
+                        , Css.boxShadow5 Css.zero (Css.px 8) (Css.px 17) Css.zero (Css.rgba 0 0 0 0.2)
+                        ]
+                    ]
+                ]
+                [ text "Randomize" ]
             ]
-        , Loading.spinner
-            [ Loading.active model.loading ]
-        , lineUp model
-        , ul [ styles [ Css.listStyleType Css.none, Css.margin Css.zero, Css.padding Css.zero, Css.displayFlex, Css.flexWrap Css.wrap, Css.justifyContent Css.center ] ]
-            (List.map renderPlayer (Model.orderedPlayers model))
-        , button
-            [ disabled (Model.isRandomizable model), onClick Randomize ]
-            [ text "Randomize" ]
         ]
 
 
-lineUp : Model -> Html Msg
-lineUp model =
-    case model.lineUp of
+lineUp : Maybe Model.LineUp -> Html Msg
+lineUp optionalLineUp =
+    case optionalLineUp of
         Nothing ->
-            div [] []
+            Html.text ""
 
         Just lineUp ->
-            div [] []
+            div
+                [ onClick ClearLineUp
+                , styles
+                    [ Css.position Css.absolute
+                    , Css.width (Css.vw 100)
+                    , Css.height (Css.vh 100)
+                    , Css.backgroundColor (Css.rgba 0 0 0 0.4)
+                    , Css.zIndex (Css.int 100)
+                    , Css.displayFlex
+                    ]
+                ]
+                [ div
+                    [ styles
+                        [ Css.backgroundColor white
+                        , Css.width (Css.vw 80)
+                        , Css.left (Css.vw 10)
+                        , Css.alignSelf Css.center
+                        , Css.position Css.relative
+                        , Css.boxShadow5 Css.zero (Css.px 12) (Css.px 15) Css.zero (Css.rgba 0 0 0 0.24)
+                        ]
+                    ]
+                    [ Html.img
+                        [ src "./assets/lineup.png"
+                        , styles
+                            [ Css.padding (Css.rem 2.5)
+                            , Css.height (Css.rem 3)
+                            , Css.property "object-fit" "contain"
+                            , Css.display Css.block
+                            , Css.margin2 Css.zero Css.auto
+                            ]
+                        ]
+                        []
+                    , div [ styles [ Css.padding (Css.rem 1) ] ]
+                        [ case lineUp of
+                            Model.Single single ->
+                                div []
+                                    [ Html.text (Model.displayName single.player1)
+                                    , Html.img
+                                        [ src "./assets/soccer-table.jpg"
+                                        , styles
+                                            [ Css.width (Css.pct 100)
+                                            , Css.property "object-fit" "contain"
+                                            ]
+                                        ]
+                                        []
+                                    , Html.text (Model.displayName single.player2)
+                                    ]
+
+                            Model.Double double ->
+                                div []
+                                    [ Html.text (Model.displayName double.teamA.player1 ++ "&" ++ Model.displayName double.teamA.player2)
+                                    , Html.img
+                                        [ src "./assets/soccer-table.jpg"
+                                        , styles
+                                            [ Css.width (Css.pct 100)
+                                            , Css.property "object-fit" "contain"
+                                            ]
+                                        ]
+                                        []
+                                    , Html.text (Model.displayName double.teamB.player1 ++ "&" ++ Model.displayName double.teamB.player2)
+                                    ]
+                        ]
+                    ]
+                ]
 
 
 errorMessage : Maybe Model.Error -> Html Msg
